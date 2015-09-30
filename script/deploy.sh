797ce7b8183d52a1874865e72bd62a3e987c3738
sean <seanhsu0102@gmail.com>
2015-09-30 15:30:00 +0800

    update

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 REPO="https://github.com/seanhsu0102/travis-broken-example.git"
-SKEY="oohay-dev.pem"
+SKEY="~/.ssh/oohay-dev.pem"
 
 ssh -i $SKEY ubuntu@54.178.203.37 git clone $REPO
