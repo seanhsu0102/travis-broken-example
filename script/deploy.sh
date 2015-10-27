e24fe33540d4eb530d727f88230008ddaab85102
sean <seanhsu0102@gmail.com>
2015-10-27 12:04:13 +0800

    update script 44444

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,8 @@ set -ev
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-if [ $(git branch|awk '{print $2}') == "master" ]; then
+BRANCH=$(git branch|awk '{print $2}')
+if [ $BRANCH == "master" ]; then
 	curl --user streetvoice:ru,g/upm,4 http://54.250.122.204:5000/deploy/test_prod
 else
 	curl --user streetvoice:ru,g/upm,4 http://54.250.122.204:5000/deploy/test
