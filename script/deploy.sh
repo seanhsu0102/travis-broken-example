92bf91bce1c2229cab035c4c87ebfd1dc5368902
sean <seanhsu0102@gmail.com>
2015-10-27 14:29:59 +0800

    LOl

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,7 +8,7 @@ BRANCH=$(git branch|sed '1d' |awk '{ print $1 }')
 
 echo "$BRANCH"
 echo "test $BRANCH == 'master'"
-if test "$BRANCH" == "master"  
+if test "$BRANCH" == 'master'
 then
 	curl --user streetvoice:ru,g/upm,4 http://54.250.122.204:5000/deploy/test_prod
 else
