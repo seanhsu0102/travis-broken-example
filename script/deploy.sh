401a758d237d29d9f6a9d5536b74b73def688271
sean <seanhsu0102@gmail.com>
2015-10-27 12:10:23 +0800

    update script 8888

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,8 +5,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 BRANCH=$(git branch|awk '{print $2}')
-echo $BRANCH
-if [ $BRANCH == "master" ]; then
+echo $(BRANCH)
+if [ "$(BRANCH)" == "master" ]; then
 	curl --user streetvoice:ru,g/upm,4 http://54.250.122.204:5000/deploy/test_prod
 else
 	curl --user streetvoice:ru,g/upm,4 http://54.250.122.204:5000/deploy/test
